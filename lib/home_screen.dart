fae89cf7bd262e09a0077877472b2debd508b4fa
Ankit Subedi <ankitsubedi89@gmail.com>
2022-07-26 08:17:52 +0545

    statelesswidget first sample

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello world'),
+      ),
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_my_first_project/home_screen.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
