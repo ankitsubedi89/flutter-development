7ce3edb8016ab90db1f7eceeabcc75e8a563318f
Ankit Subedi <ankitsubedi89@gmail.com>
2022-08-04 11:24:16 +0545

    signup page demo

--- lib/views/components/my_field.dart
+++ lib/views/components/my_field.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class MyField extends StatelessWidget {
+  final TextEditingController controller;
   final String text;
-  const MyField({Key? key, required this.text}) : super(key: key);
+  final myValidator;
+  const MyField(
+      {Key? key,
+      required this.text,
+      this.myValidator,
+      required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(text, textAlign: TextAlign.left,style: TextStyle(fontSize: 15),)
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TextFormField(
+            obscureText: false,
+            controller: controller,
+            decoration: InputDecoration(
+              labelText: "Enter your $text",
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+              
+            ),
+            validator: (value){
+              if(value!.isEmpty){
+                return "Field cannot be empty";
+              }
+              return myValidator(value);
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/views/pages/signup_page.dart
+++ lib/views/pages/signup_page.dart
@@ -1,27 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_my_first_project/views/components/my_button.dart';
+import 'package:flutter_my_first_project/views/components/my_field.dart';
 
 class SignupPage extends StatelessWidget {
-  final name = TextEditingController(); 
-   SignupPage({Key? key}) : super(key: key);
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final addressController = TextEditingController();
+  final phoneController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  SignupPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Enter your name",
-                  border: OutlineInputBorder(
-                      borderSide: const BorderSide(width: 3, color: Colors.yellow),
-                      borderRadius: BorderRadius.circular(50.0)),
+        child: Form(
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                MyField(
+                  text: 'Name',
+                  controller: nameController,
                 ),
-              ),
-            )
-          ],
+                MyField(
+                  text: 'Address',
+                  controller: addressController,
+                ),
+                MyField(
+                  text: 'Phone Number',
+                  controller: phoneController,
+                ),
+                MyField(
+                  text: 'Email',
+                  controller: emailController,
+                  myValidator: (value) {
+                    if (!value.toString().contains("@")) {
+                      return "Email is not valid";
+                    }
+                    return null;
+                  },
+                ),
+                MyField(
+                  text: 'Password',
+                  controller: passwordController,
+                ),
+                MyField(
+                  text: 'Confirm Password',
+                  controller: confirmController,
+                ),
+                MyButton(
+                  onTap: () {
+                    var isFormValid = formKey.currentState!.validate();
+                    if (isFormValid) {
+                      var data = {
+                        "name": nameController.text,
+                        "address": addressController.text,
+                        "phone number": phoneController.text,
+                        "email": emailController.text,
+                        "passowrd": passwordController.text,
+                        "confirm password": confirmController.text,
+                      };
+                      print(data);
+                    }
+                  },
+                  text: "Submit",
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
